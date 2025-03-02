502620294680143a723d3323b1453ca7e7e9c42a
shuochen <shuo.chen@campus.lmu.de>
2025-03-02 16:05:32 +0100

    add setup-evn sh memo

---
+++ setup-env.sh
@@ -0,0 +1,9 @@
+# for smoagents
+# first fork the official repo then clone my forked version.
+# this should be done in ../smolagents-cs folder
+git clone git@github.com:chenxshuo/smolagents-cs.git
+cd smolagents-cs
+git remote add upstream https://github.com/huggingface/smolagents.git
+git remote -v
+git checkout -b cs-deep-research
+
