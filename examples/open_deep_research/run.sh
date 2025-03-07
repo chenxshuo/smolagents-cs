65fd948e5b0558d0777ea3659b84be20b0f7a35b
Shuo Chen <chenss@amazon.de>
2025-03-07 17:22:56 +0100

    update scripts

--- examples/open_deep_research/run.sh
+++ examples/open_deep_research/run.sh
@@ -1,1 +1,5 @@
 python run.py --model-id "openai/gpt-4o" "What is the capital of China?"
+
+python run.py --model-id "Qwen/QwQ-32B-Preview" --provider="hf" question="What is the capital of China?"
+
+python run_gaia.py --run-name="test gaia"
